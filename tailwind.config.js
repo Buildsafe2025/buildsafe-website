c509d147050218b7765eeb63985abea6bb95f904
Joseph Oluwayemi <joejoeoyemi@gmail.com>
2026-01-15 22:17:43 +0100

    chore: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const config =  {
+   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        md: "2rem",
+        lg: "3rem",
+      },
+      screens: {
+        sm: "375px",
+        md: "768px",
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1440px',
+      },
+    },
+  },
+};
+
+export default config
